41dd33ffceea44de83b3cddb89309da4ef360261
Junaid jd <junaidsaleem.ctn@gmail.com>
2022-11-08 01:17:07 +0500

    Complete

--- src/Activity6-Table/utility/tabs.js
+++ src/Activity6-Table/utility/tabs.js
@@ -1,14 +1,14 @@
 
 async function fetchData() {
     var url = "https://jsonplaceholder.typicode.com/users";
-    await fetch(url)
-        .then(response => response.json())
-        .then(json => {
-            console.log(json)
-            if (json.length > 0) {
-                json.forEach(user =>
-                    document.getElementById('tableBody').innerHTML +=
-                    `
+    let response = await fetch(url)
+    let json = await response.json()
+
+    console.log(json)
+    if (json.length > 0) {
+        json.forEach(user =>
+            document.getElementById('tableBody').innerHTML +=
+            `
                     <tr>
                     <td>${user.id}</td>
                     <td>${user.name}</td>
@@ -16,9 +16,7 @@ async function fetchData() {
                     <td>${user.address.street}, ${user.address.city}, zip - ${user.address.zipcode}</td>
                 </tr>
                     `
-                )
-            }
-        })
-        .catch(error => console.log(error));
+        )
+    }
 }
 
