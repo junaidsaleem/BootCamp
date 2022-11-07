f852fd90d2bb2020e61c2c9833d6eb2e1f1e5391
Junaid jd <junaidsaleem.ctn@gmail.com>
2022-11-08 00:41:09 +0500

    Activity 6 Table

---
+++ src/Day11Activity6-Table/utility/tabs.js
@@ -0,0 +1,23 @@
+
+function fetchData() {
+    var url = "https://jsonplaceholder.typicode.com/users";
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            if (json.length > 0) {
+                json.forEach(user =>
+                    document.getElementById('tableBody').innerHTML +=
+                    `
+                    <tr>
+                    <td>${user.name}</td>
+                    <td>${user.email}</td>
+                    <td>${user.address.street}, ${user.address.city}, zip - ${user.address.zipcode}</td>
+                </tr>
+                    `
+                )
+            }
+        })
+        .catch(error => console.log(error));
+}
+
